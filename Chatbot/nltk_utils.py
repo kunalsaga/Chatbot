ca100138624ceae29b06514a1f62da18ad1ab973
kunalsaga <kunalsagavakar@gmail.com>
2022-05-02 17:33:03 +0200

    initail commit

---
+++ Chatbot/nltk_utils.py
@@ -0,0 +1,22 @@
+# import nltk
+
+# nltk.download('punkt')
+
+from nltk.stem.porter import PorterStemmer
+
+Stemmer=PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return Stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence,all_words):
+    pass
+
+a="Hello, thanks for visiting"
+print(a)
+a=tokenize(a)
+print(a)
+
